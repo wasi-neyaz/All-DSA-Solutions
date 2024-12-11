0d854608f99edb9139c4cd60ca94eede7d37b799
Wasiqah Neyaz <135358544+wasi-neyaz@users.noreply.github.com>
2024-12-11 23:33:41 +0530

    Merge_without_extra_space

---
+++ Sorting/Merge_without_extra_space.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        int n = a.size();
+        int m = b.size();
+        int left = n-1;
+        int right = 0;
+        
+        while(left >=0 && right < m){
+            if(a[left] >= b[right]){
+                swap(a[left],b[right]);
+                left--;
+                right++;
+            }
+            else{
+                break;
+            }
+        }
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        
+    }
+};
